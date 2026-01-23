d924c2d36b8bc25560d41f62c3920ad1c6e20ae9
Rituja Sinha <ritujasinha2@gmail.com>
2026-01-23 20:00:27 +0530

    Restrict CORS origin to localhost:5173

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
 
 
 app.use(express.json());
